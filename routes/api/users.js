78f13955960e73d5612f1336cf7e3b97e4c1db37
mehmet sacli <sacli@outlook.com>
2018-07-18 12:19:22 +0300

    post register implement and gravatar users route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
 
 // @route get api/users/test
 //@desc tests users route
@@ -9,5 +12,46 @@ const router = express.Router();
 router.get('/test', (req,res)=>{
     res.json({msg: "Users Works"});
 });
+
+// @route get api/users/register
+//@desc register users route
+//@access Public
+
+router.post('/register', (req,res)=>{
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if(user) {
+                return res.status(400).json({email: "Email already exists"});
+            } else {
+                const avatar = gravatar.url(req.body.email, {
+                    s: '200', // size
+                    r: 'pg', // rating
+                    d: 'mm' // default
+                });
+
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    // avatar: avatar, es6 sayesinde direk avatar
+                    // diye yazilabilir. bu arada gravatar diye bir servis internetten veriyor tanımlı
+                    avatar,
+                    password: req.body.password
+                });
+
+                bcrypt.genSalt(10, (err , salt) => {
+                    bcrypt.hash(newUser.password, salt, (err,hash)=> {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        newUser.save()
+                            .then(user=> res.json(user))
+                            .catch(err => console.log(err));
+                    });
+                });
+            }
+        });
+        
+});
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 // require routes
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+// body parser middleware
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys').mongoURI;
