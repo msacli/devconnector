e250636728204c045d57f15f465ea4a469e08970
sacli@outlook.com <sacli@outlook.com>
2018-07-26 21:23:19 +0300

    deneme ev

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
-// require routes
+// require routes asd
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
